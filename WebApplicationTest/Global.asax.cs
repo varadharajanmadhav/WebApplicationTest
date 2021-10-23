4aa47d0f4a03a8b500cba3576bb1b6cc02199fd7
Varadharajan <varadharajan@vegam.co>
2021-10-23 16:04:30 +0530

    sdasdasdasd

--- WebApplicationTest/Global.asax.cs
+++ WebApplicationTest/Global.asax.cs
@@ -13,7 +13,7 @@ namespace WebApplicationTest
     {
         void Application_Start(object sender, EventArgs e)
         {
-            // Code that runs on application startup
+            // Code that runs on application startup asdasdasd
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
